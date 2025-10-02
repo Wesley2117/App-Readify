6f4fd7ef9207af8c7cbb467f574b7bd149925b15
Wesley2117 <wesleysant741@gmail.com>
2025-10-02 20:00:42 -0300

    Alteracao no nome do arquivo de loginMobile pra Readify

--- src/modules/bookchoice/screens/bookChoice.js
+++ src/modules/bookchoice/screens/bookChoice.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
 import Header from "../components/header";
-import { FlatList } from "react-native-gesture-handler";
 
 export default function bookChoice(){
     return(
